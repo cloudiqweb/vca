123f290ca4c213ae169d7241232ffd85473fbd14
cloudiqweb <vaidhyanathan@cloudiq.io>
2017-09-15 15:28:33 -0700

    Add files via upload

---
+++ vca/build_update.ps1
@@ -0,0 +1,51 @@
+﻿[CmdletBinding()]
+Param(
+    
+    [Parameter(Mandatory=$True)]
+    [string]$Environment,
+
+    [Parameter(Mandatory=$True)]
+    [string]$Version,
+
+    [Parameter(Mandatory=$False)]
+    [string]$GAAccount
+
+
+)
+
+$Build_Location = 'C:\inetpub\VCAChargeCapture';
+
+$BaseRef = 'VCAChargeCapture';
+
+$ConfigFolderPath = $Build_Location + '\assets\config\';
+
+IF($BaseRef -and $BaseRef -ne '') {
+
+    $htmlFilePath = $Build_Location + '\index.html';
+    $htmlContent = Get-Content $htmlFilePath;
+    $htmlContent = $htmlContent -replace '<base href="/">', ('<base href="/' + $BaseRef + '/">');
+
+    IF($GAAccount -and $GAAccount -ne '') {
+        $htmlContent = $htmlContent -replace "var gaTrackingCode = '';", ("var gaTrackingCode = '" + $GAAccount + "';");
+    }
+
+    Set-Content $htmlFilePath $htmlContent;
+    Write-Host "Updated BaseHref and Ga Account";
+}
+
+IF($Environment -and $Environment -ne '') {
+
+    $configJson = Get-Content ($ConfigFolderPath + 'env.json') -raw | ConvertFrom-Json;
+    $configJson.env = $Environment;
+    $configJson | ConvertTo-Json  | set-content ($ConfigFolderPath + 'env.json');
+    Write-Host "Updated Environment";
+}
+
+IF($Environment -and $Environment -ne '' -and $Version -and $Version -ne '') {
+
+    $configPath = ($ConfigFolderPath + 'config.' + $Environment + '.json');
+    $configJson = Get-Content  $configPath -raw | ConvertFrom-Json;
+    $configJson.Version_Number = 'V ' + $Version;
+    $configJson | ConvertTo-Json  | set-content $configPath;
+    Write-Host "Updated Version";
+}
